adfbc9522d6d69798ccb34160d100014b780c6b2
moradalkhatib <muradalkhateb@icloud.com>
2021-10-09 22:07:11 +0300

    startTest

--- components/thirdScreen.js
+++ components/thirdScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { View, Text,StyleSheet } from 'react-native'
 import Button from './Buttons'
-export default function thirdScreen({ navigation }) {
+export default function thirdScreen({ navigation,route }) {
+    // const [data, setData] = useState([])
+    let getRandom=(min, max)=> {
+        
+      return Math.floor(Math.random() * (max - min + 1) + min); 
+      }
+      const number=getRandom(0,route.params.params.joke.length-1)
+
+    useEffect(()=>{
+        
+      console.log(route.params.params.joke)
+      console.log(route.params.params.joke[0].joke)
+    } ,[route.params])
+
     return (
         <View style={styles.container}>
-            <Text>this is the 3 screen</Text>
-           <Button title='back to secound screen' onPress={()=>navigation.navigate('SecoundScreen')} ></Button>
+            <Text style={styles.textStyle}>{route.params.params.joke[number].joke}</Text> 
+           <Button title='back to secound screen' onPress={()=>navigation.navigate('first')} ></Button>
         </View>
     )
 }
@@ -19,4 +32,13 @@ const styles = StyleSheet.create({
   
   
   },
+  textStyle: {
+    textAlign: 'center', // <-- the magic
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 0,
+    marginLeft:'30%',
+    width: 200,
+    color:"white"
+  }
 })
